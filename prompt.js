86d72886a712e3b79595943a6bf12d7e2f87e487
David Irvine <dave@daveirvine.me>
2013-10-28 21:55:10 +0000

    Refactor to fix incompatibility with charm (and therefore multimeter).

--- prompt.js
+++ prompt.js
@@ -1,11 +1,12 @@
 /* jshint node: true */
 "use strict";
 
-var colors = require("colors"),
+var charm = require("charm")(process),
+	colors = require("colors"),
 	EventEmitter = require("events").EventEmitter,
 	Prompt,
 	promptInstance = null,
-	readline = require("readline"),
+	stream = require("stream"),
 	util = require("util");
 
 colors.setTheme({
@@ -15,92 +16,67 @@ colors.setTheme({
 });
 
 (function () {
-	var closeInterface,
-		openInterface,
-		prompt,
-		rl,
-		sigintHandler;
+	var prompt;
 
 	Prompt = function Prompt() {
+		var stdinBuffer = new Buffer("", "utf8"),
+			writeableStream;
+
 		prompt = this;
 
 		EventEmitter.call(prompt);
 
-		prompt.interfaceOpen = false;
+		writeableStream = new stream.Writable();
+		writeableStream._write = function (chunk, encoding, callback) {
+			if (chunk.readInt8(0) !== 13) {
+				stdinBuffer = Buffer.concat([stdinBuffer, chunk]);
+			} else {
+				prompt.emit("line", stdinBuffer.toString());
+				stdinBuffer = new Buffer("", "utf8");
+			}
+			callback();
+		};
+
+		process.stdin.pipe(writeableStream);
+
 		prompt.sigint = false;
+		prompt.charm = charm;
+
+		charm.removeAllListeners("^C");
+		charm.on("^C", function () {
+			prompt.sigint = true;
+			charm.write("\n");
+			process.exit();
+		});
 	};
 
 	util.inherits(Prompt, EventEmitter);
 
 	Prompt.prototype.choice = function (question, choices, callback) {
-		var asker;
+		(function asker() {
+			charm.write(question + "\n");
+			choices.forEach(function (choice, index) {
+				charm.write("  " + index + ") " + choice + "\n");
+			});
 
-		openInterface();
-
-		rl.write(question + "\n");
-		choices.forEach(function (choice, index) {
-			rl.write("  " + index + ") " + choice + "\n");
-		});
-
-		asker = function () {
-			rl.prompt();
-			rl.once("line", function (input) {
+			prompt.once("line", function (input) {
 				var index = input.trim() * 1;
 				if (prompt.sigint) {
 					return;
 				}
 
 				if (!/^[0-9]+$/.test(input) || isNaN(index) || index < 0 || index >= choices.length) {
-					rl.write("Try again:".warn);
-					rl.write("\n");
+					charm.write("\nTry again:".error);
+					charm.write("\n");
 					asker();
 				} else {
 					callback(null, {
 						"index": index,
 						"value": choices[index]
 					});
-
-					closeInterface();
 				}
 			});
-		};
-
-		asker();
-	};
-
-	sigintHandler = function () {
-		prompt.sigint = true;
-		rl.write("\n");
-		
-		closeInterface();
-	};
-
-	closeInterface = function () {
-		if (!prompt.interfaceOpen) {
-			return;
-		}
-
-		rl.close();
-		rl.removeListener("SIGINT", sigintHandler);
-
-		prompt.interfaceOpen = false;
-		prompt.sigint = false;
-	};
-
-	openInterface = function () {
-		if (prompt.interfaceOpen) {
-			return;
-		}
-
-		rl = readline.createInterface({
-			"input": process.stdin,
-			"output": process.stdout
-		});
-
-		rl.on("SIGINT", sigintHandler);
-
-		prompt.interfaceOpen = true;
-		prompt.sigint = false;
+		}());
 	};
 })();
 
